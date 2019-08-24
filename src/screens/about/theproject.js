c546e5310be19c2c4f0a31520986746cc287e80e
Timmy Xiao <timmyxiao1@gmail.com>
2019-08-24 02:53:09 -0400

    align about project

--- src/screens/about/theproject.js
+++ src/screens/about/theproject.js
@@ -4,8 +4,8 @@ import { Content, Text } from 'native-base';
 export default class TheProject extends React.Component{
     render() {
         return(
-            <Content>
-                <Text>MITcoin is a charity project run by MIT MindHandHeart. It is our mission to empower charitable causes by rewarding good deeds performed around MIT's campus.</Text>
+            <Content style={{flex: 1, paddingTop: 30}}>
+                <Text style={{fontSize: 18, textAlign: 'center'}}>MITcoin is a charity project run by MIT MindHandHeart. It is our mission to empower charitable causes by rewarding good deeds performed around MIT's campus.</Text>
             </Content>
         )
     }
