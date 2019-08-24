7356611cd02c48ee87eb1a424308d02b29b57416
Timmy Xiao <timmyxiao1@gmail.com>
2019-08-24 01:33:09 -0400

    beautify about team

--- src/screens/about/team.js
+++ src/screens/about/team.js
@@ -1,30 +1,90 @@
 import React from 'react';
-import { Content, Text } from 'native-base'
+import { StyleSheet } from 'react-native';
+import { Content, Card, CardItem, Text, H2 } from 'native-base';
 
 export default class TheTeam extends React.Component{
     render(){
         return(
-            <Content>
-                <Text>W. Craig Carter</Text>
-                <Text style={{textAlign:'center'}}>POSCO Professor of Materials Science and Engineering</Text>
-                <Text>MacVicar Fellow</Text>
-                <Text>MITcoin Creator</Text>
-                <Text>{'ccarter@mit.edu' + '\n'}</Text>
-                <Text>Mary Beth Wagner</Text>
-                <Text>Graduate Student</Text>
-                <Text>Department of Materials Science and Engineering</Text>
-                <Text>{'mbwagner@mit.edu' + '\n'}</Text>
-                <Text>Maise O'Brien</Text>
-                <Text style={{textAlign:'center'}}>Communications and Community Engagement Manager at MindHandHeart</Text>
-                <Text>{'maisieob@mit.edu' + '\n' }</Text>
-                <Text>Issac Redlon </Text>
-                <Text>Web Developer</Text>
-                <Text>EECS</Text>
-                <Text>{"iredlon@mit.edu" + '\n'}</Text>
-
-                <Text>Timmy Xiao</Text>
-                <Text>mitimmy@mit.edu</Text>
+            <Content style={styles.container}>
+                <Card>
+                    <CardItem header>
+                        <H2>W. Craig Carter</H2>
+                    </CardItem>
+                    <CardItem style={styles.cardItemList}>
+                        <Text>POSCO</Text>
+                    </CardItem>
+                    <CardItem style={styles.cardItemList}>
+                        <Text>Professor of Materials Science and Engineering</Text>
+                    </CardItem>
+                    <CardItem style={styles.cardItemList}>
+                        <Text>MacVicar Fellow</Text>
+                    </CardItem>
+                    <CardItem style={styles.cardItemList}>
+                        <Text>MITcoin Creator</Text>
+                    </CardItem>
+                    <CardItem footer>
+                        <Text>ccarter@mit.edu</Text>
+                    </CardItem>
+                </Card>
+                <Card>
+                    <CardItem header>
+                        <H2>Mary Beth Wagner</H2>
+                    </CardItem>
+                    <CardItem style={styles.cardItemList}>
+                        <Text>Graduate Student</Text>
+                    </CardItem>
+                    <CardItem style={styles.cardItemList}>
+                        <Text>Department of Materials Science and Engineering</Text>
+                    </CardItem>
+                    <CardItem footer>
+                        <Text>mbwagner@mit.edu</Text>
+                    </CardItem>
+                </Card>
+                <Card>
+                    <CardItem header>
+                        <H2>Maisie O'Brien</H2>
+                    </CardItem>
+                    <CardItem style={styles.cardItemList}>
+                        <Text>Communications and Community Engagement Manager at MindHandHeart</Text>
+                    </CardItem>
+                    <CardItem footer>
+                        <Text>maisieob@mit.edu</Text>
+                    </CardItem>
+                </Card>
+                <Card>
+                    <CardItem header>
+                        <H2>Issac Redlon</H2>
+                    </CardItem>
+                    <CardItem style={styles.cardItemList}>
+                        <Text>Web Developer</Text>
+                    </CardItem>
+                    <CardItem style={styles.cardItemList}>
+                        <Text>EECS</Text>
+                    </CardItem>
+                    <CardItem footer>
+                        <Text>iredlon@mit.edu</Text>
+                    </CardItem>
+                </Card>
+                <Card>
+                    <CardItem header>
+                        <H2>Timmy Xiao</H2>
+                    </CardItem>
+                    <CardItem footer>
+                        <Text>mitimmy@mit.edu</Text>
+                    </CardItem>
+                </Card>
             </Content>
-        )
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 8,
+    },
+    cardItemList: {
+        paddingTop: 0,
+        paddingBottom: 5
+    }
+});
