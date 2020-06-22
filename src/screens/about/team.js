197a006eac0a3e7adb211d2be58a0b4371251824
Brian Ntanga <brianntanga@dhcp-18-21-201-145.dyn.mit.edu>
2020-06-22 16:49:25 -0400

    improved team page views

--- src/screens/about/team.js
+++ src/screens/about/team.js
@@ -1,90 +1,80 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Content, Card, CardItem, Text, H2 } from 'native-base';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Content, Card, Text, H2 } from "native-base";
+import AboutCard from "./aboutCard.js";
 
-export default class TheTeam extends React.Component{
-    render(){
-        return(
-            <Content style={styles.container}>
-                <Card>
-                    <CardItem header>
-                        <H2>W. Craig Carter</H2>
-                    </CardItem>
-                    <CardItem style={styles.cardItemList}>
-                        <Text>POSCO</Text>
-                    </CardItem>
-                    <CardItem style={styles.cardItemList}>
-                        <Text>Professor of Materials Science and Engineering</Text>
-                    </CardItem>
-                    <CardItem style={styles.cardItemList}>
-                        <Text>MacVicar Fellow</Text>
-                    </CardItem>
-                    <CardItem style={styles.cardItemList}>
-                        <Text>MITcoin Creator</Text>
-                    </CardItem>
-                    <CardItem footer>
-                        <Text>ccarter@mit.edu</Text>
-                    </CardItem>
-                </Card>
-                <Card>
-                    <CardItem header>
-                        <H2>Mary Beth Wagner</H2>
-                    </CardItem>
-                    <CardItem style={styles.cardItemList}>
-                        <Text>Graduate Student</Text>
-                    </CardItem>
-                    <CardItem style={styles.cardItemList}>
-                        <Text>Department of Materials Science and Engineering</Text>
-                    </CardItem>
-                    <CardItem footer>
-                        <Text>mbwagner@mit.edu</Text>
-                    </CardItem>
-                </Card>
-                <Card>
-                    <CardItem header>
-                        <H2>Maisie O'Brien</H2>
-                    </CardItem>
-                    <CardItem style={styles.cardItemList}>
-                        <Text>Communications and Community Engagement Manager at MindHandHeart</Text>
-                    </CardItem>
-                    <CardItem footer>
-                        <Text>maisieob@mit.edu</Text>
-                    </CardItem>
-                </Card>
-                <Card>
-                    <CardItem header>
-                        <H2>Issac Redlon</H2>
-                    </CardItem>
-                    <CardItem style={styles.cardItemList}>
-                        <Text>Web Developer</Text>
-                    </CardItem>
-                    <CardItem style={styles.cardItemList}>
-                        <Text>EECS</Text>
-                    </CardItem>
-                    <CardItem footer>
-                        <Text>iredlon@mit.edu</Text>
-                    </CardItem>
-                </Card>
-                <Card>
-                    <CardItem header>
-                        <H2>Timmy Xiao</H2>
-                    </CardItem>
-                    <CardItem footer>
-                        <Text>mitimmy@mit.edu</Text>
-                    </CardItem>
-                </Card>
-            </Content>
-        );
-    }
+export default class TheTeam extends React.Component {
+  render() {
+    craig_body = (
+      <View>
+        <Text style={styles.cardItemList}>POSCO</Text>
+        <Text style={styles.cardItemList}>
+          Professor of Materials Science and Engineering
+        </Text>
+        <Text style={styles.cardItemList}>MacVicar Fellow</Text>
+        <Text style={styles.cardItemList}>MITcoin Creator</Text>
+        <Text style={styles.email}>ccarter@mit.edu</Text>
+      </View>
+    );
+
+    mary_body = (
+      <View>
+        <Text style={styles.cardItemList}>Graduate Student</Text>
+        <Text style={styles.cardItemList}>
+          Department of Materials Science and Engineering
+        </Text>
+        <Text style={styles.email}>mbwagner@mit.edu</Text>
+      </View>
+    );
+
+    maisie_body = (
+      <View>
+        <Text style={styles.cardItemList}>
+          Communications and Community Engagement Manager at MindHandHeart
+        </Text>
+
+        <Text style={styles.email}>maisieob@mit.edu</Text>
+      </View>
+    );
+    issac_body = (
+      <View>
+        <Text style={styles.cardItemList}>Web Developer</Text>
+
+        <Text style={styles.cardItemList}>EECS</Text>
+        <Text style={styles.email}>iredlon@mit.edu</Text>
+      </View>
+    );
+
+    timmy_body = <Text style={styles.email}>mitimmy@mit.edu</Text>;
+    brian_body = (
+      <View>
+        <Text style={styles.cardItemList}>Software Developer</Text>
+
+        <Text style={styles.cardItemList}>EECS</Text>
+        <Text style={styles.email}>bntanga@mit.edu</Text>
+      </View>
+    );
+    return (
+      <Content style={styles.container}>
+        <AboutCard title="W. Craig Carter" body={craig_body} />
+        <AboutCard title="Mary Beth Wagner" body={mary_body} />
+        <AboutCard title="Maisie O'Brien" body={maisie_body} />
+        <AboutCard title="Issac Redlon" body={issac_body} />
+        <AboutCard title="Timmy Xiao" body={timmy_body} />
+        <AboutCard title="Brian Ntanga" body={brian_body} />
+      </Content>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 8,
-    },
-    cardItemList: {
-        paddingTop: 0,
-        paddingBottom: 5
-    }
+  container: {
+    flex: 1,
+    padding: 8,
+  },
+  cardItemList: {
+    paddingTop: 0,
+    paddingBottom: 5,
+  },
+  email: { fontWeight: "600", paddingTop: 8 },
 });
